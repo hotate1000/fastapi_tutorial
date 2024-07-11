dd77b6d5740fc529d6aba295d30ca2115a4982cd
s-tsujino <s-tsujino@sungrove.co.jp>
2024-07-11 10:29:17 +0900

    現状コミット

--- src/main.py
+++ src/main.py
@@ -1,7 +1,9 @@
 import jwt
-from fastapi import FastAPI, Depends, HTTPException, Request
 import secrets
-from pydantic import BaseModel, Field, model_validator, ValidationError
+from fastapi import FastAPI, HTTPException, Request, Security
+from fastapi.security import HTTPBearer
+from pydantic import BaseModel, Field, model_validator
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 from typing import Dict
 
 app = FastAPI()
@@ -9,6 +11,9 @@ app = FastAPI()
 # JWTの設定
 SECRET_KEY = secrets.token_hex(32)
 ALGORITHM = "HS256"
+
+# Bearer認証スキームを設定する
+security = HTTPBearer()
 
 
 class TokenData(BaseModel):
@@ -31,47 +36,72 @@ class TokenData(BaseModel):
         return values
 
 
-# トークンを生成する関数（JWSを利用）
-def create_access_token(data: dict):
-    return jwt.encode(data, SECRET_KEY, algorithm=ALGORITHM)
-
-
-# トークンを検証する関数
-def verify_token(token: str):
-    try:
-        # トークンをデコードし、検証。ペイロードを取得
-        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-        return payload
-    except jwt.PyJWTError:
-        raise HTTPException(status_code=401, detail="トークン認証エラー")
-
 @app.post("/token")
 def generate_token(token_data: TokenData):
     """
-    トークンを生成する
+    トークンを生成するエンドポイント
 
     Parameters
     ----------
     token_data: TokenData
         トークンの値
     """
-    access_token = create_access_token(token_data.data)
+    access_token = __create_access_token(token_data.data)
     return {"access_token": access_token}
 
 
-# トークンを検証するエンドポイント
 @app.get("/verify-token")
-def check_token(request: Request):
-    token = request.headers.get("Authorization")
-    if token:
-        token = token.split(" ")[1]  # "Bearer " の部分を除去
-        payload = verify_token(token)
-        return {"valid": True, "payload": payload}
-    else:
-        raise HTTPException(status_code=400, detail="Authorization header missing")
+def check_token(
+    request: Request,
+    credentials: HTTPAuthorizationCredentials = Security(security)
+):
+    """
+    _summary_
+
+    Parameters
+    ----------
+    request: Request
+        クライアントからのリクエスト情報
+    credentials : HTTPAuthorizationCredentials, optional
+        Bearerトークンを含む認証情報を取得。Security(security)によって、認証スキームを適応
+    """
+    # トークンを取得
+    token = credentials.credentials
+    payload = verify_token(token)
+    return {"valid": True, "payload": payload}
 
 
 # エンドポイントの確認
 @app.get("/")
 def read_root():
     return {"message": "Hello World"}
+
+
+# トークンを生成する関数（JWSを利用）
+def __create_access_token(data: dict):
+    """
+    JWSを利用してトークンを作成
+
+    Parameters
+    ----------
+    token: dict
+        トークン情報
+    """
+    return jwt.encode(data, SECRET_KEY, algorithm=ALGORITHM)
+
+
+def verify_token(token: str):
+    """
+    トークンを検証
+
+    Parameters
+    ----------
+    token: str
+        トークンの情報
+    """
+    try:
+        # トークンをデコードし、検証。ペイロードを取得
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        return payload
+    except jwt.PyJWTError:
+        raise HTTPException(status_code=401, detail="トークン認証エラー")
